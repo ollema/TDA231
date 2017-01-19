647792e6f48fc2d665f989529c59916a0c3dcb33
Olle Månsson <olle.lars.mansson@gmail.com>
2017-01-19 10:44:47 +0100

    Added code for p22

--- HW0/main.m
+++ HW0/main.m
@@ -1,4 +1,5 @@
-
+%%
+% Problem 2.1
 f=@(x)function_p21(x);
 ezplot(@(x,y)f([x,y]'),1)
 %t=linspace(-2,2,100);
@@ -18,3 +19,40 @@ ezplot(@(x,y)f([x,y]',1))
 
 %I dont know if ezplot is the best method to use to plot the level curves
 %look up other methtods.... contour plots
+
+%%
+% Problem 2.2
+clf
+clc
+clear
+dataSet = load('dataset0.txt');
+covariance = cov(dataSet);
+correlation = corrcoef(dataSet);
+scaleFactor = max(dataSet(:));
+scaledSet = dataSet ./ scaleFactor;
+scaledCovar = cov(scaledSet);
+scaledCorr = corrcoef(scaledSet);
+
+hold on
+colormap jet
+
+subplot(2,2,1)
+imagesc(covariance)
+colorbar
+title('\fontsize{10pt}\rm{Covariance of dataset0 - }\it{\bf{X}}')
+
+subplot(2,2,2)
+imagesc(correlation)
+colorbar
+title('\fontsize{10pt}\rm{Correlation of dataset0 - }\it{\bf{X}}')
+
+subplot(2,2,3)
+imagesc(scaledCovar)
+colorbar
+title('\fontsize{10pt}\rm{Covariance of scaled dataset0 - }\it{\bf{Y}}')
+
+subplot(2,2,4)
+imagesc(scaledCorr)
+colorbar
+title('\fontsize{10pt}\rm{Correlation of scaled dataset0 - }\it{\bf{Y}}')
+
