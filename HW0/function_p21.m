dcf895426a39a77e91f7877e7ebe0efba6137bd8
Axel Löf <lof.axel@gmail.com>
2017-01-19 09:45:13 +0100

    Vectorized function_p21

--- HW0/function_p21.m
+++ HW0/function_p21.m
@@ -1,6 +1,7 @@
-function [ out ] = function_p21(x,r)
+function [ out ] = function_p21(x)
+    nData=size(x,2);
     mu=[1,1]';
     sigma=[0.1 -0.05;-0.05 0.2];
-    out=(x-mu)'*(inv(sigma))*(x-mu)/2-r;
+    out=0.5*sum( (x-repmat(mu,1,nData)).*( inv(sigma)*(x-repmat(mu,1,nData)) ) );
 end
 
--- HW0/main.m
+++ HW0/main.m
@@ -1,6 +1,13 @@
-%f=@(x,y,r)
-hold on
-f=@(x,r)function_p21(x,r);
-ezplot(@(x,y)f([x,y]',1))
+f=@(x)function_p21(x);
+ezplot(@(x,y)f([x,y]'),1)
+%t=linspace(-2,2,100);
+%[X,Y]=meshgrid(t,t);
+%Z=f([X,Y]')
 %I dont know if ezplot is the best method to use to plot the level curves
 %look up other methtods.... contour plots
+
+%%
+A=[1,0;0,1];
+x=[1,1;2,1;3,1]';
+%%
+f([1,1]')
