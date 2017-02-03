60918d552782ad46a9684e128f4cede6f2d056c2
Olle Månsson <olle.lars.mansson@gmail.com>
2017-02-03 09:13:48 +0100

    Updated 2.1 a,b,c

--- HW2/sph_bayes.m
+++ HW2/sph_bayes.m
@@ -1,21 +1,13 @@
-function [p1, p2, Ytest] = sph_bayes(x, y, xTest)
-% label out the data
-x1 = x(y > 0);
-x2 = x(y < 0);
-
-% find mean and covariance
-mu1 = mean(x1);
-mu2 = mean(x2);
-cov1 = cov(x1);
-cov2 = cov(x2);
-
+function [p1, p2, Ytest] = sph_bayes(xTest, mu1, mu2, cov1, cov2)
 % calculate "likelihoods"
-pre1 = gaussian(xTest, mu1, cov1);
-pre2 = gaussian(xTest, mu2, cov2);
+pre1 = mvnpdf(xTest, mu1, cov1);
+pre2 = mvnpdf(xTest, mu2, cov2);
 
 % return the goods
 p1 = pre1/(pre1 + pre2);
 p2 = pre2/(pre1 + pre2);
 probs = [p1, p2];
 [~,Ytest] = max(probs);
+% fix class
+Ytest = Ytest - (Ytest-1)*3;
 end
