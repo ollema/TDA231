3b52f9fb3bbe505a68794da1b21dd2eade9685bd
Axel Löf <lof.axel@gmail.com>
2017-02-04 16:30:15 +0100

    fixed errors

--- HW2/CrossValidationScript.m
+++ HW2/CrossValidationScript.m
@@ -1,4 +1,4 @@
-nFractions=5;
+nFractions=2;
 nDataX1=size(x1,1);
 nDataX2=size(x2,1);
 
@@ -12,7 +12,7 @@ for i=1:nFractions
     verificationDataX1=x1(LLX1:ULX1,:);
     trainingDataX1=x1;
     trainingDataX1(LLX1:ULX1,:)=[];
-    classX1=(1:size(verificationDataX1,1))';
+    classX1=ones(size(verificationDataX1,1),1);
     mu1 = mean(trainingDataX1);
     cov1 = cov(trainingDataX1);
     
@@ -21,7 +21,7 @@ for i=1:nFractions
     verificationDataX2=x2(LLX2:ULX2,:);
     trainingDataX2=x2;
     trainingDataX2(LLX2:ULX2,:)=[];
-    classX2=-(1:size(verificationDataX2,1))';
+    classX2=-ones(size(verificationDataX2,1),1);
     mu2 = mean(trainingDataX2);
     cov2 = cov(trainingDataX2);
     
@@ -29,8 +29,8 @@ for i=1:nFractions
     predictionClass1=new_classifier(verificationData,mu1,mu2);
     [~,~,predictionClass2] =sph_bayes(verificationData,mu1,mu2,cov1,cov2);
     classes=[classX1;classX2];
-    percentageError1=percentageError1+nnz(classes==predictionClass1)/(nFractions*size(classes,1));
-    percentageError2=percentageError2+nnz(classes==predictionClass2)/(nFractions*size(classes,1));
+    percentageError1=percentageError1+nnz(classes~=predictionClass1)/(nFractions*size(classes,1));
+    percentageError2=percentageError2+nnz(classes~=predictionClass2)/(nFractions*size(classes,1));
 end
 disp(percentageError1)
 disp(percentageError2)
