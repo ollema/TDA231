b7d168e020abff3094a52daf117d257606a6d420
Olle Månsson <olle.lars.mansson@gmail.com>
2017-02-23 21:31:05 +0100

    Finished solution to p2.1

--- HW4/main2.m
+++ HW4/main2.m
@@ -6,26 +6,34 @@ x = data.X;
 y = data.Y;
 
 %% Train SVM
-clc
-svm = svmtrain(x,y,'AutoScale',false, 'ShowPlot', true);
-bias = svm.Bias
-%pause(1)
-gscatter(x(:,1),x(:,2),y)
+clc, clf
 hold on
+% train the svm
+svm = svmtrain(x,y,'AutoScale',false, 'ShowPlot', false, 'BoxConstraint', 1);
+% bias and support vectors are given by the model directly
+bias = svm.Bias
 sv = svm.SupportVectors;
-plot(sv(:,1),sv(:,2),'ko','MarkerSize',7)
+% first, plot the support vectors
+plot(sv(:,1),sv(:,2),'ko','MarkerSize',10, 'LineWidth', 1)
+% then, plot the point which could not be separated
+plot(sv(2,1),sv(2,2),'rx','MarkerSize',18, 'LineWidth', 2)
+% plot all of the points with class
+gscatter(x(:,1),x(:,2),y,['r' 'b'])
 
+% calculate weights from alpha * support vectors
 w1 = dot(svm.Alpha, sv(:,1));
 w2 = dot(svm.Alpha, sv(:,2));
-bias = svm.Bias;
+% soft margin from eq. 5.12
+soft_margin = 2/norm([w1 w2])
 
-% or with line given as y = a*x + b
+% slope of hyperplane
 a = -w1/w2;
 b = -svm.Bias/w2;
+
 xx = linspace(-4,4);
 yy = a*xx + b;
-plot(xx, yy, '-k')
-n = 2;
-plot(sv(n,1),sv(n,2),'ko','MarkerSize',8)
-plot(sv(n,1),sv(n,2),'ko','MarkerSize',9)
-plot(sv(n,1),sv(n,2),'ko','MarkerSize',10)
+yl = a*xx + b - soft_margin/2*sqrt(1+a^2);
+yu = a*xx + b + soft_margin/2*sqrt(1+a^2);
+plot(xx, yy, '-k', 'LineWidth', 1)
+plot(xx, yl, '--k', 'LineWidth', 1)
+plot(xx, yu, '--k', 'LineWidth', 1)
