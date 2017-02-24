976f1bd0e7cf800ddc22da97f539a31449e6f301
Olle Månsson <olle.lars.mansson@gmail.com>
2017-02-24 16:10:04 +0100

    Update to p2.1

--- HW4/main2.m
+++ HW4/main2.m
@@ -9,14 +9,12 @@ y = data.Y;
 clc, clf
 hold on
 % train the svm
-svm = svmtrain(x,y,'AutoScale',false, 'ShowPlot', false, 'BoxConstraint', 1);
+svm = svmtrain(x,y,'AutoScale',false, 'BoxConstraint', 1);
 % bias and support vectors are given by the model directly
 bias = svm.Bias
 sv = svm.SupportVectors;
 % first, plot the support vectors
 plot(sv(:,1),sv(:,2),'ko','MarkerSize',10, 'LineWidth', 1)
-% then, plot the point which could not be separated
-plot(sv(2,1),sv(2,2),'rx','MarkerSize',18, 'LineWidth', 2)
 % plot all of the points with class
 gscatter(x(:,1),x(:,2),y,['r' 'b'])
 
@@ -37,3 +35,12 @@ yu = a*xx + b + soft_margin/2*sqrt(1+a^2);
 plot(xx, yy, '-k', 'LineWidth', 1)
 plot(xx, yl, '--k', 'LineWidth', 1)
 plot(xx, yu, '--k', 'LineWidth', 1)
+
+
+%%
+% then, any point which could not be separated
+plot(sv(2,1),sv(2,2),'rx','MarkerSize',18, 'LineWidth', 2)
+
+
+%% Problem 2.2 a)
+
