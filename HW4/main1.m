ee5b962e0161f410f1222af0cc7dd75ad552135e
Axel Löf <lof.axel@gmail.com>
2017-02-27 18:44:12 +0100

    merge

--- HW4/main1.m
+++ HW4/main1.m
@@ -3,13 +3,12 @@ clc
 A=[4,4,1;2,2,1;4,0,1;0,-2,-1;0,0,-1;-2,0,-1];
 b=[1,1,1,1,1,1]';
 f=@(X)0.5*(X(1)^2+X(2)^2);
-sol=fmincon(f,[0,0,0]',A,-b)
-
+sol=-fmincon(f,[0,0,0]',A,-b)
+  
 %% Problem 1.2d
 X=[4,4;2,2;4,0;0,2;2,0;0,0]';
 t=[1,1,1,-1,-1,-1];
 A=zeros(6,6);
-%loopkungen 
 for i=1:6
     for j=1:6
         A(i,j)=(X(:,i)'*X(:,j))*(t(i)*t(j));
@@ -18,10 +17,10 @@ end
 vect=[1,1,1,1,1,1]';
 f=@(alpha)-(vect'*alpha-0.5*alpha'*(A*alpha))
 initial=rand(6,1)*0.1;
-sol=fmincon(f,initial,-eye(6),zeros(6,1),t,0)
+alpha=fmincon(f,initial,-eye(6),zeros(6,1),t,0)
 w=zeros(2,1);
 for i=1:6
-    w=w+sol(i)*t(i)*X(:,i);
+    w=w+alpha(i)*t(i)*X(:,i);
 end
 b=t(5)-w'*X(:,5);
 w
