b88abe6e4133f4ff1a4236119749cb78c6b3b4bd
Axel Löf <lof.axel@gmail.com>
2017-03-01 17:30:01 +0100

    Fixed algorithm

--- HW5/KMeansCluster.m
+++ HW5/KMeansCluster.m
@@ -1,41 +1,67 @@
-function [out,musAt] = KMeansCluster(kernel,data,initialMus,retrieveDataAt)
+function [out,musAt] = KMeansCluster(k,kernel,data,...
+    initialClassAssignments,retrieveDataAt)
 
-    mus=initialMus;
-    distances=GetDistances(data,mus,kernel);%implement
-    classAssignments=CalculateClassAssignments(distances);%implement
+    %define k
+    classAssignments=initialClassAssignments;
+    %distances=GetDistances(data,classAssignments,kernel);%implement
     ClassAssignmentsChanged=true;
-    musAt=cell(0);
+    classAt=cell(0);
     
     iterationCounter=0;
     while ClassAssignmentsChanged
-        iterationCounter = iterationCounter+1;
-        %update the mu vectors        
-        distances=GetDistances(data,mus,kernel);%calculate new distances
+        iterationCounter = iterationCounter+1;       
+        distances=GetDistances(k,data,classAssignments,kernel);%calculate new distances
         newClassAssignments = ClassAssignments(distances);%make new classAssignments
         ClassAssignmentsChanged = IsClassAssignmentsChanged(...
             newClassAssignments,classAssignments);
         classAssignments=newClassAssignments;
         if nnz(iterationCounter==retrieveDataAt)
-            musAt{1,numel(musAt)+1}=classAssignments;
-            musAt{2,numel(mustAt)+1}=mus;
+            classAt{1,numel(classAt)+1}=iterationCounter;
+            classAt{2,numel(classAt)+1}=classAssignments;
         end
     end
     
     out=cell(0);
-    out{1,1}=classAssignments;
-    out{2,1}=mus;
+    out{1,1}=iterationCounter;
+    out{2,1}=classAssignments;
 
 end
 
 %% Internal funcitons, not supposed to be used anywhere else than in KMeansCluster
 
-%implement
 function [ClassAssignments] = CalculateClassAssignments(distances)
-    ClassAssignments = 0;
+    [~,ClassAssignments] = min(distances);
 end
 
-%implement
 function [ClassAssignmentsChanged] = IsClassAssignmentsChanged(...
     newClassAssignment,classAssignment)
-    ClassAssignmentsChanged=true;
+    ClassAssignmentsChanged=isequal(newClassAssignment,classAssignment);
+end
+
+%implement. TODO
+function [distances] = GetDistances(k,data,classAssignments,kernel)
+    nData=size(data,2);
+    distances=zeros(k,nData);
+    %generate K-matrix
+    K=zeros(nData);
+    for i=1:nData
+        for j=1:nData
+            K(i,j)=kernel(data(:,i),data(:,j));
+            
+        end
+    end
+    
+    Z=zeros(nData,k);
+    for c=1:numel(classAssignments)
+        Z(c,classAssignments(c))=1;
+    end
+    
+    for n=1:nData
+        for k_=1:k
+            %implement.
+            distances(n,k_)=K(n,n)...
+                -2*(1/N)*dot(K(k_,:), Z(:,k_)) + ;
+        end
+    end
+        
 end
