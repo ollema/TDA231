00e4f02529d058391d78bf8a81500a49c207116b
Olle Månsson <olle.lars.mansson@gmail.com>
2017-03-02 14:05:04 +0100

    Started on 1.2

--- HW5/main1.m
+++ HW5/main1.m
@@ -29,3 +29,27 @@ toc
 gscatter(X(1,:), X(2,:), kmc{2,1})
 
 % profile viewer
+
+
+%% Problem 1.2
+clc, clear
+data = load('medium_100_10k.mat');
+vocab = data.vocab;
+wordemb = data.wordembeddings;
+
+[~, ~, ~, D] = kmeans(wordemb, 10);
+ 
+%% a)
+clc
+[~, I] = sort(D);
+closestWords = vocab(I(1:10,:));
+disp(closestWords)
+
+%% b)
+clc, clear D, clear closestWords, clear I
+[~, ~, ~, D] = kmeans(wordemb, 10, 'Replicates', 1);
+
+%%
+
+
+
